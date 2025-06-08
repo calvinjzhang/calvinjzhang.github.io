c4f9afbf99f5b3d5f5f2f93f8e12c0c0bb8ffca2
calvinjzhang <73315019+calvinjzhang@users.noreply.github.com>
2025-06-08 08:01:19 +0000

    Deploying to gh-pages from @ calvinjzhang/calvinjzhang.github.io@11b84ad23967718f52d622a6f52ab152f81fd1a9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-art",
+          title: "art",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/art/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
